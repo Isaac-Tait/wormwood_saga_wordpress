dfebb71137d3b4b491d6753fb0525041d0aca7cc
Isaac Tait <isaac@mountaintopcoding.com>
2022-06-12 21:14:24 -0400

    chore: clear husky commit warning 🐶

--- pages/novel/index.js
+++ pages/novel/index.js
@@ -1,31 +1,32 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
-import { getAllPosts } from '../../lib/api'
+//import { getAllPosts } from '../../lib/api'
 
 const Novel = ({ allPosts: { edges } }) => (
-    <div className={styles.container}>
-      <Head>
-        <title>Blog articles page</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-  
-      <main>
-        <h1>Latest blog articles</h1>
-        <hr />
-        <section>
-          {edges.map(({ node }) => (
-            <div key={node.id}>
-              <div>
-                <h2>{node.title}</h2>
-                <Link href={`/blog/${node.slug}`}>
-                  <a>Read more</a>
-                </Link>
-              </div>
-            </div>
-          ))}
-        </section>
-      </main>
-    </div>
-  );
+  <div>
+    <Head>
+      <title>Blog articles page</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-  export default Novel;
+    <main>
+      <h1>Latest blog articles</h1>
+      <hr />
+      <section>
+        {edges.map(({ node }) => (
+          <div key={node.id}>
+            <div>
+              <h2>{node.title}</h2>
+              <Link href={`/blog/${node.slug}`}>
+                <a>Read more</a>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  </div>
+);
+
+export default Novel;
